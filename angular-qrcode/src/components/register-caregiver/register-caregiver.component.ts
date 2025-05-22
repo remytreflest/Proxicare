e3859ef5d6e570865c7fc272541d6a4b6ae5e33e
treflest remy <mrtreflestremy@outlook.com>
2025-05-22 10:25:03 +0200

    feat(components): adding some components

---
+++ angular-qrcode/src/components/register-caregiver/register-caregiver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-caregiver',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-caregiver.component.html',
+  styleUrl: './register-caregiver.component.scss'
+})
+export class RegisterCaregiverComponent {
+
+}
