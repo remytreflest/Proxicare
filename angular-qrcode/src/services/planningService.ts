c9e1847aa9b79b6e4998d98fad037c174e3b8251
Alexandre <duquesne.alexandre21@gmail.com>
2025-07-02 18:31:46 +0200

    on m'a dit de commit donc je commit

--- angular-qrcode/src/services/planningService.ts
+++ angular-qrcode/src/services/planningService.ts
@@ -5,26 +5,8 @@ import { BehaviorSubject, catchError, map, Observable, of } from 'rxjs';
 import { environment } from '../environment';
 import { AuthService } from '@auth0/auth0-angular';
 import { RolesEnum } from '../resources/rolesEnum';
+import { Appointment } from '../models/appointement';
 
-export interface Appointement {
-  Id: number;
-  Title: string;
-  Start: Date;
-  End: Date;
-  CaregiverID : number;
-  PatientID : number;
-  Description: string;
-  Acts: Act[];
-}
-
-export interface Act {
-  Id: number;
-  Name: string;
-  Description: string;
-  Price: number;
-  CreatedAt: Date;
-  UpdatedAt: Date;
-}
 
 @Injectable({
   providedIn: 'root'
@@ -34,11 +16,11 @@ export class PlanningService {
 
   constructor(private http: HttpClient, private router: Router, private auth: AuthService) {}
 
-  getAppointementsByUserId(userId: string): Observable<Appointement[]> {
-    return this.http.get<Appointement[]>(`${environment.urls.back}/planning/${userId}`).pipe(
+  getAppointements(): Observable<Appointment[]> {
+    return this.http.get<Appointment[]>(`${environment.urls.back}/appointments/`).pipe(
       catchError((err) => {
         console.error('Erreur lors de la récupération des rendez-vous', err);
-        return of([] as Appointement[]);
+        return of([] as Appointment[]);
       })
     );
   }
