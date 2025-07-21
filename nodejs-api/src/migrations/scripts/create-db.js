2f6df72ff75a3285365cc9c7b483faa872f1b3cb
unknown <mrtreflestremy@outlook.com>
2025-07-21 15:11:25 +0200

    fix

--- nodejs-api/src/migrations/scripts/create-db.js
+++ nodejs-api/src/migrations/scripts/create-db.js
@@ -1,24 +1,24 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-const client = new Client({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: 'db',
-  port: 5432,
-  database: 'postgres', // Connexion à postgres pour pouvoir créer une autre base
-});
+// const client = new Client({
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   host: 'db',
+//   port: 5432,
+//   database: 'postgres', // Connexion à postgres pour pouvoir créer une autre base
+// });
 
-const dbName = process.env.POSTGRES_DB;
+// const dbName = process.env.POSTGRES_DB;
 
-client.connect()
-  .then(() => client.query(`CREATE DATABASE ${dbName};`))
-  .then(() => console.log(`Database "${dbName}" created.`))
-  .catch(err => {
-    if (err.code === '42P04') {
-      console.log(`Database "${dbName}" already exists.`);
-    } else {
-      console.error('Error creating database:', err);
-    }
-  })
-  .finally(() => client.end());
+// client.connect()
+//   .then(() => client.query(`CREATE DATABASE ${dbName};`))
+//   .then(() => console.log(`Database "${dbName}" created.`))
+//   .catch(err => {
+//     if (err.code === '42P04') {
+//       console.log(`Database "${dbName}" already exists.`);
+//     } else {
+//       console.error('Error creating database:', err);
+//     }
+//   })
+//   .finally(() => client.end());
